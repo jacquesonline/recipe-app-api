734449d7553bbeaedcd8e82d37c2b5c2f5270b7d
jacquesonline <j.c.steenkamp10@gamil.com>
2023-12-11 16:43:28 +1100

    Add tests for admin URL

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,9 +7,24 @@ from app import calc
 # test calculate module
 class CalcTests(SimpleTestCase):
     ''' Test that two numbers are added together '''
+
     def test_add_number(self):
 
         self.assertEqual(calc.add(3, 8), 11)
+
     def test_subtract_numers(self):
         res = calc.subtract(10, 15)
         self.assert_(res, 5)
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class TestUrls(TestCase):
+
+    def test_admin_url(self):
+        client = Client()
+        response = client.get(reverse('admin:index'))
+        status_code = response.status_code
+        print(f'Status code: {status_code}')
+        self.assertEqual(response.status_code, 200)
