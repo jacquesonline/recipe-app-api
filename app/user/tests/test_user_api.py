c95a885f34674bd282c0d5f600dd62be12f986fa
jacquesonline <j.c.steenkamp10@gamil.com>
2023-12-13 19:06:58 +1100

    Fix user API status code

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -33,7 +33,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
