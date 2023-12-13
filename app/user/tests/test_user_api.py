438a437382c6f6ef0ed02b678451b6c48b5b1047
jacquesonline <j.c.steenkamp10@gamil.com>
2023-12-13 19:14:00 +1100

    Fix password validation in user creation API test

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -51,17 +51,17 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_password_too_short(self):
+    def test_password_too_short_error(self):
         """Test an error is issued if the password is less than 5"""
         payload = {
             'email': 'test@example.com',
-            'password': 'testpass123',
-            'mail': 'Test Name',
+            'password': 'pw',
+            'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
-        ).exists
+        ).exists()
         self.assertFalse(user_exists)
 
